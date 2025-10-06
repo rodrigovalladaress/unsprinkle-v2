43b6187cc43d01fb0d96538cf2ef7259eeece4d6
Rodrigo <dim3trodon@gmail.com>
2025-10-06 16:12:24 +0100

    feat: use different image formats

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,10 +1,31 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const Hero = () => {
   return (
     <Wrapper>
-      <HeroImage src="/images/hero-img.jpg" />
+      <picture>
+        <source
+          type="image/avif"
+          srcSet="
+            /images/hero-img.avif 1x,
+            /images/hero-img@2x.avif 2x,
+            /images/hero-img@3x.avif 3x
+          "
+        />
+        <source
+          type="image/jpg"
+          srcSet="
+            /images/hero-img.jpg 1x,
+            /images/hero-img@2x.jpg 2x,
+            /images/hero-img@3x.jpg 3x
+          "
+        />
+        <HeroImage
+          src="/images/hero-img.jpg"
+          alt="A photo of a cat looking at you"
+        />
+      </picture>
       <Swoop src="/swoop.svg" />
     </Wrapper>
   );
